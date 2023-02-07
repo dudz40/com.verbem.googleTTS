bf5c6d98cc980e898dbf9f2771ee4f5d3b4490d9
Martin Verbeek <dudz40@gmail.com>
2023-02-07 13:23:09 +0100

    3.0.1

--- app.js
+++ app.js
@@ -19,41 +19,21 @@ class googleTTS extends Homey.App {
 
     onInit() {
         this.log(`${Homey.manifest.id} running...`);
+        this.foundDevices = [];
 
-        const FoundDevices = [];
+        this.discoverDevices();
+        this.goWithTheFlow();
+    }
 
-        FoundDevices['Broadcast'] = {name: 'Broadcast', description: 'Broadcast to all devices'};
+    findDeviceIP(device) {
+        for (const device_data in this.foundDevices)
+            if (this.foundDevices[device_data].name == device.name || (this.foundDevices[device_data].id != undefined && this.foundDevices[device_data].id == device.id))
+                return { ip: this.foundDevices[device_data].host, port:this.foundDevices[device_data].port };
 
-        const discoveryStrategy = this.homey.discovery.getStrategy('googlecast');
-        const initialResults = discoveryStrategy.getDiscoveryResults();
+        return "0.0.0.0";
+    }
 
-        Object.entries(initialResults).forEach(([key, discoveryResult]) => {
-            if (discoveryResult.txt.md !== 'Google Cast Group') {
-                this.log('Discovery result', JSON.stringify(discoveryResult, ' ', 4));
-                const db = {};
-                db.id = discoveryResult.id;
-                db.host = discoveryResult.address;
-                db.port = discoveryResult.port;
-                db.name = discoveryResult.txt.fn;
-                db.description = discoveryResult.txt.md;
-
-                FoundDevices[discoveryResult.id] = db;
-            }
-        });
-
-        discoveryStrategy.on('result', discoveryResult => {
-            if (discoveryResult.txt.md !== 'Google Cast Group') {
-                this.log('Change in discovery result', JSON.stringify(discoveryResult, ' ', 4));
-                const db = {};
-                db.id = discoveryResult.id;
-                db.host = discoveryResult.address;
-                db.port = discoveryResult.port;
-                db.name = discoveryResult.txt.fn;
-                db.description = discoveryResult.txt.md;
-                FoundDevices[discoveryResult.id] = db;
-            }
-        });
-
+    async goWithTheFlow() {
         this.homey.flow.getActionCard('tts')
         .registerRunListener((args, state) => {
             return new Promise((resolve, reject) => {
@@ -61,43 +41,55 @@ class googleTTS extends Homey.App {
                 device.setLanguage(args.language);
                 if (args.speed) device.setTtsSpeed(args.speed);
                 if (args.muted) device.setSendToMuted(args.muted);
-                device.setVolume(args.volume / 100.0)
+                device.setVolume(args.volume)
                 device.setTts(args.text);
 
                 if (args.device.name === 'Broadcast') {
-                    for (let device_data in FoundDevices) {
-                        if (FoundDevices[device_data].name !== 'Broadcast') {
-                            device.setIp(FoundDevices[device_data].host);
+                    for (let device_data in this.foundDevices) {
+                        if (this.foundDevices[device_data].name !== 'Broadcast') {
+                            device.setPort(this.foundDevices[device_data].port);
+                            device.setIp(this.foundDevices[device_data].host);
                             device.audio();
                         }
                     }
                 } else {
-                    let ip = this.findDeviceIP(FoundDevices, args.device);
+                    let { ip, port } = this.findDeviceIP(args.device);
 
                     if (ip == undefined || ip == "0.0.0.0") return reject();
 
                     device.setIp(ip);
+                    device.setPort(port);
                     device.audio();
                 }
                 return resolve();
             });
-        }).registerArgumentAutocompleteListener('device', (query, args) => {
+        })
+        .registerArgumentAutocompleteListener('device', (query, args) => {
             const devices = []
-
-            for (const device_data in FoundDevices)
-                if (FoundDevices[device_data].name.toLowerCase().indexOf(query.toLowerCase()) > -1 || FoundDevices[device_data].description.toLowerCase().indexOf(query.toLowerCase()) > -1)
-                    devices.push(FoundDevices[device_data]);
+            for (const device_data in this.foundDevices)
+                if (this.foundDevices[device_data].name.toLowerCase().indexOf(query.toLowerCase()) > -1 || this.foundDevices[device_data].description.toLowerCase().indexOf(query.toLowerCase()) > -1)
+                    devices.push(this.foundDevices[device_data]);
 
             return Promise.resolve(devices);
-        });
+        })
     }
 
-    findDeviceIP(FoundDevices, device) {
-        for (const device_data in FoundDevices)
-            if (FoundDevices[device_data].name == device.name || (FoundDevices[device_data].id != undefined && FoundDevices[device_data].id == device.id))
-                return FoundDevices[device_data].host;
+    async discoverDevices() {
+        this.foundDevices['Broadcast'] = {name: 'Broadcast', description: 'Broadcast to all devices'};
 
-        return "0.0.0.0";
+        const discoveryStrategy = this.homey.discovery.getStrategy('googlecast');
+        const initialResults = discoveryStrategy.getDiscoveryResults();
+
+        discoveryStrategy.on('result', discoveryResult => {
+            this.log('Discovery result(ON)', discoveryResult.txt.fn, 'on', discoveryResult.address, discoveryResult.port);             
+            const db = {};
+            db.id = discoveryResult.id;
+            db.host = discoveryResult.address;
+            db.port = discoveryResult.port;
+            db.name = discoveryResult.txt.fn;
+            db.description = discoveryResult.txt.md;
+            this.foundDevices[discoveryResult.id] = db;
+        });
     }
 
 }
